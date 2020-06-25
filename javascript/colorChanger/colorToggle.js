7cd9342a6b3da4302d82dd940ea1207d870bb4a0
Risyardi Iswanto <elyeel@yahoo.com>
2020-06-25 14:42:18 -0700

    going to refactor toggler

---
+++ javascript/colorChanger/colorToggle.js
@@ -0,0 +1,15 @@
+let whiteColour = true;
+let button = document.querySelector("button");
+//solution 1
+// button.addEventListener("click", () => {
+//   if (whiteColour) {
+//     document.bgColor = "purple";
+//   } else {
+//     document.bgColor = "white";
+//   }
+//   whiteColour = !whiteColour;
+// });
+
+button.addEventListener("click", () => {
+  document.body.classList.toggle("purple");
+});
