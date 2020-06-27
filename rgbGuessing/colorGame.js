6bd879ad31293328e95f857125cae980fc2e1a59
Risyardi Iswanto <elyeel@yahoo.com>
2020-06-26 17:51:56 -0700

    added random logic

--- rgbGuessing/colorGame.js
+++ rgbGuessing/colorGame.js
@@ -7,11 +7,16 @@ let colours = [
   "rgb(255, 0, 255)",
 ];
 
+const pickColour = function() {
+  let random = Math.floor(Math.random() * colours.length);
+  return colours[random];
+}
+
 let squares = document.querySelectorAll(".square");
-let pickedColour = colours[3];
+let pickedColour = pickColour();
 let colourDisplay = document.getElementById("colourDisplay");
 colourDisplay.textContent = pickedColour;
-
+let messageDisplay = document.getElementById("message"); 
 for (let i=0; i<squares.length; ++i) {
   // add initial colour
   squares[i].style.backgroundColor = colours[i];
@@ -22,9 +27,18 @@ for (let i=0; i<squares.length; ++i) {
     let clickedColour = squares[i].style.backgroundColor;
     // compare colour to clickedColour
     if (clickedColour === pickedColour) {
-      alert("Correct");
+      messageDisplay.textContent = "Correct";
+      changeColour(pickedColour);
     } else {
-      alert("WRONG!");
+      squares[i].style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   })
 }
+
+const changeColour = (colour) => {
+  for (let i = 0; i < squares.length; ++i) {
+    squares[i].style.backgroundColor = colour;
+  }
+};
+
