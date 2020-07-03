80baa8010b34f1201cc411dbaba1b5a025ed05c2
Risyardi Iswanto <elyeel@yahoo.com>
2020-07-02 22:37:32 -0700

    added some exercise

---
+++ javascript/jQuery/exercise/practice.js
@@ -0,0 +1,10 @@
+// select all divs and give them a purple background
+$("div").css(
+  "background", "purple"
+);
+
+$(".highlight").css("width", "200px");
+
+$("#third").css("border", "5px solid orange");
+
+$("div:first-of-type").css("color", "pink");
