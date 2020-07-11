73e7a84966f42ad75c416e28ebb8445fd3bd2d2c
Risyardi Iswanto <elyeel@yahoo.com>
2020-07-10 19:01:41 -0700

    completed basic functionality, going into styling

--- javascript/toDoListProject/assets/js/toDos.js
+++ javascript/toDoListProject/assets/js/toDos.js
@@ -1,15 +1,25 @@
 // check off specific todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   //if li is gray
   $(this).toggleClass("completed");
 });
 
 // click on x to delete to do
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   $(this)
     .parent()
     .fadeOut(500, function () {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    // grabbing new todo text from input box
+    let toDoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append(`<li><span>X </span>${toDoText}</li>`);
+  }
 });
