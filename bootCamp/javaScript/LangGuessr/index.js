fa73c332b5a26fdf37591109d2528d6177545d64
Risyardi Iswanto <elyeel@yahoo.com>
2021-04-15 12:52:16 -0700

    added try-catch to LangGuessr index.js to mitigate langcode not recognized by langs library

---
+++ bootCamp/javaScript/LangGuessr/index.js
@@ -0,0 +1,20 @@
+// REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
+const franc = require('franc');
+const langs = require('langs');
+const colors = require('colors');
+
+const input = process.argv[2];
+const langCode = franc(input);
+if (langCode === 'und') {
+	console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red);
+} else {
+	try {
+		const language = langs.where('3', langCode);
+		console.log(`Our best guess is: ${language.name}`.green);
+	} catch (error) {
+		console.error(
+			`Got an error from langs library: ${error} - Language Code:${langCode.green}`
+				.red
+		);
+	}
+}
