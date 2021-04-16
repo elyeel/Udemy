c90fc997f3b550d664ed04bae2632379be95d868
Risyardi Iswanto <elyeel@yahoo.com>
2021-04-15 17:10:03 -0700

    created simple routing to express server

--- bootCamp/projects/express/firstApp/index.js
+++ bootCamp/projects/express/firstApp/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(() => {
-	console.log('We got something');
+// app.use((req, res) => {
+//   console.log('We got something');
+
+// });
+
+app.get('/cats', (req, res) => {
+	res.send('<h1>Meowww!</h1>');
+});
+
+app.get('*', (req, res) => {
+	//get anything, should be the last
+	res.send("I don't know that path");
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}!`));
