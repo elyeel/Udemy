fb3715679c390a145da646ad29c9ca8d7a5d3982
Risyardi Iswanto <elyeel@yahoo.com>
2021-05-13 14:13:02 -0700

    updating, deleting, inserting from mongoose

--- bootCamp/mongoDB/MongooseBasics/index.js
+++ bootCamp/mongoDB/MongooseBasics/index.js
@@ -27,13 +27,13 @@ const Movie = mongoose.model('Movie', movieSchema);
 // 	rating: 'R'
 // });
 
-Movie.insertMany([
-	{ title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
-	{ title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
-	{ title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
-	{ title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
-	{ title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
-]).then((data) => {
-	console.log('IT WORKED!');
-	console.log(data);
-});
+// Movie.insertMany([
+// 	{ title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+// 	{ title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+// 	{ title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+// 	{ title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+// 	{ title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+// ]).then((data) => {
+// 	console.log('IT WORKED!');
+// 	console.log(data);
+// });
