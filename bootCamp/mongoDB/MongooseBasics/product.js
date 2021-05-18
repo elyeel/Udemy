e22021e65438152db2d5190a1d74f7838b3b4d11
Risyardi Iswanto <elyeel@yahoo.com>
2021-05-17 20:13:27 -0700

    mongoose instance methods and static practice

--- bootCamp/mongoDB/MongooseBasics/product.js
+++ bootCamp/mongoDB/MongooseBasics/product.js
@@ -1,44 +1,47 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    })
+mongoose
+	.connect('mongodb://localhost:27017/shopApp', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('CONNECTION OPEN!!!');
+	})
+	.catch((err) => {
+		console.log('OH NO ERROR!!!!');
+		console.log(err);
+	});
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 20
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: [0, 'Price must be positive ya dodo!']
-    },
-    onSale: {
-        type: Boolean,
-        default: false
-    },
-    categories: [String],
-    qty: {
-        online: {
-            type: Number,
-            default: 0
-        },
-        inStore: {
-            type: Number,
-            default: 0
-        }
-    },
-    size: {
-        type: String,
-        enum: ['S', 'M', 'L']
-    }
-
+	name: {
+		type: String,
+		required: true,
+		maxlength: 20
+	},
+	price: {
+		type: Number,
+		required: true,
+		min: [ 0, 'Price must be positive ya dodo!' ]
+	},
+	onSale: {
+		type: Boolean,
+		default: false
+	},
+	categories: [ String ],
+	qty: {
+		online: {
+			type: Number,
+			default: 0
+		},
+		inStore: {
+			type: Number,
+			default: 0
+		}
+	},
+	size: {
+		type: String,
+		enum: [ 'S', 'M', 'L' ]
+	}
 });
 
 // productSchema.methods.greet = function () {
@@ -46,54 +49,68 @@ const productSchema = new mongoose.Schema({
 //     console.log(`- from ${this.name}`)
 // }
 
-productSchema.methods.toggleOnSale = function () {
-    this.onSale = !this.onSale;
-    return this.save();
-}
+productSchema.methods.toggleOnSale = function() {
+	this.onSale = !this.onSale;
+	return this.save();
+};
 
+productSchema.methods.addCategory = function(newCat) {
+	this.categories.push(newCat);
+	return this.save();
+};
 
-productSchema.methods.addCategory = function (newCat) {
-    this.categories.push(newCat);
-    return this.save();
-}
-
-productSchema.statics.fireSale = function () {
-    return this.updateMany({}, { onSale: true, price: 0 })
-}
-
+productSchema.statics.fireSale = function() {
+	return this.updateMany({}, { onSale: true, price: 0 });
+};
 
 const Product = mongoose.model('Product', productSchema);
 
-
 const findProduct = async () => {
-    const foundProduct = await Product.findOne({ name: 'Mountain Bike' });
-    console.log(foundProduct)
-    await foundProduct.toggleOnSale();
-    console.log(foundProduct)
-    await foundProduct.addCategory('Outdoors')
-    console.log(foundProduct)
-}
+	const foundProduct = await Product.findOne({ name: 'Mountain Bike' });
+	console.log(foundProduct);
+	await foundProduct.toggleOnSale();
+	console.log(foundProduct);
+	await foundProduct.addCategory('Seminar');
+	console.log(foundProduct);
+};
 
-// Product.fireSale().then(res => console.log(res))
+// Product.fireSale().then((res) => console.log(res));
 
 // findProduct();
 
+// const bike = new Product({
+// 	name: 'Mountain Bike',
+// 	price: 599,
+// 	categories: [ 'Cycling' ],
+// 	size: 'L'
+// });
+// bike
+// 	.save()
+// 	.then((data) => {
+// 		console.log('Saved one');
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO UERRRORRR!');
+// 		console.log(err);
+// 	});
 
-
-
-
-
-
-// const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'XS' })
-// bike.save()
-//     .then(data => {
-//         console.log("IT WORKED!")
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log("OH NO ERROR!")
-//         console.log(err)
-//     })
+// const bike = new Product({
+// 	name: 'Cycling Jersey',
+// 	price: 28.5,
+// 	categories: [ 'Cycling' ],
+// 	size: 'S'
+// });
+// bike
+// 	.save()
+// 	.then((data) => {
+// 		console.log('IT WORKED!');
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO ERROR!');
+// 		console.log(err);
+// 	});
 
 // Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 9 }, { new: true, runValidators: true })
 //     .then(data => {
@@ -104,5 +121,3 @@ const findProduct = async () => {
 //         console.log("OH NO ERROR!")
 //         console.log(err)
 //     })
-
-
